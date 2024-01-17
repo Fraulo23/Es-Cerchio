e3efa1ebf29b7f33f3cba8652176756425ec8205
Fraulo23 <fraulinifederico@gmail.com>
2024-01-17 17:03:43 +0100

    Aggiungere i file di progetto.

---
+++ Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Cerchio
+{
+    internal class Cerchio
+    {
+        public float Raggio { get; set; }
+        public Cerchio(float raggio)
+        {
+            Raggio = raggio;
+        }
+        public float Area()
+        {
+            return (float)(Math.PI * Raggio * Raggio);
+        }
+        public float Perimetro()
+        {
+            return (float)(Math.PI * Raggio*2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float r = float.Parse(Console.ReadLine());
+            Cerchio c= new Cerchio(r);
+            Console.WriteLine(c.Area());
+            Console.WriteLine(c.Perimetro());
+            Console.ReadLine();
+        }
+    }
+}
